13f54346a13837d12790436d6055e598521c080a
liamarguedas <iliamftw2013@gmail.com>
2023-07-20 12:09:01 -0300

    Add (training algorithm)

---
+++ train.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+def train(X, y, weights, biases, observations, learning_rate = 0.02, epochs = 100, verbose = True, return_weights_biases = True):
+    
+    for epoch in range(epochs):
+        
+        outputs = np.dot(X, weights) + biases
+        
+        deltas = outputs - y
+        
+        loss = (np.sum(deltas ** 2) / 2) / observations
+        
+        if verbose:
+            
+            print(f"Epoch: {epoch}/{epochs}: loss {loss}")
+            
+        deltas_scaled = deltas / observations
+        
+        weights = weights - learning_rate * np.dot(X.T, deltas_scaled)
+        
+        biases = biases - learning_rate * np.sum(deltas_scaled)
+    
+    if return_weights_biases:    
+    
+        return weights, biases
