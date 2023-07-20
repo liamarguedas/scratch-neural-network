5b4df01a245962252e1e972609060002cc8c9333
liamarguedas <iliamftw2013@gmail.com>
2023-07-20 12:00:55 -0300

    Add (Generate_data.py)

---
+++ generate_data.py
@@ -0,0 +1,15 @@
+import numpy as numpy
+
+def generate_data(observations = 1_000, return_targets = False):
+    
+    xs = np.random.uniform(low = -10, high = 10, size = (observations, 1))
+    
+    zs = np.random.uniform(low = -10, high = 10, size = (observations, 1))
+    
+    noise = np.random.uniform(low = -1, high = 1, size = (observations, 1))
+    
+    if return_targets:
+    
+        return np.column_stack((xs, zs)), (2 * xs) - (3 * zs) + 5 + noise
+    
+    return np.column_stack((xs, zs))
